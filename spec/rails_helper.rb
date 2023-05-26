f9d89c8edc7e6ba1d209d169ae8780345b51bd01
Katie Lonsdale <katie952@gmail.com>
2023-05-26 13:26:01 -0600

    configure simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,11 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter "spec/testable.rb"
+end
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
