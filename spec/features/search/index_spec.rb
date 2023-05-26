2703f396e48490d9b81fb3c91ff84da495f9c8c0
Katie Lonsdale <katie952@gmail.com>
2023-05-26 14:51:58 -0600

    refactor: add within blocks

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -8,40 +8,40 @@ RSpec.describe 'search index page' do
       click_button("Search For Members")
 
       within("#residents") do
-        expect(page).to have_content("Residents: 100")
+        expect(page).to have_content("Residents: 97")
       end
     end
     it 'has a list of the first 25 members of the Fire Nation' do
       visit root_path
       select "Fire Nation", from: "nation"
       click_button("Search For Members")
-      # within("#character-5cf5679a915ecad153ab68fd") do
-        expect(page).to have_content("Name: Afiko")
-      # end
+      within("#character-5cf5679a915ecad153ab68fd") do
+        expect(page).to have_content("Name: Chan (Fire Nation admiral)")
+      end
     end
     it 'has a list of allies and enemies' do
       visit root_path
       select "Fire Nation", from: "nation"
       click_button("Search For Members")
-      # within("#character-5cf5679a915ecad153ab68fd") do
-        # within("#allies") do
+      within("#character-5cf5679a915ecad153ab68fd") do
+        within("#allies") do
           expect(page).to have_content("Ozai")
-        # end
-        # within("#enemies") do
+        end
+        within("#enemies") do
           expect(page).to have_content("Earth Kingdom")
-        # end
-      # end
+        end
+      end
 
     end
     it 'lists their affiliation' do
       visit root_path
       select "Fire Nation", from: "nation"
       click_button("Search For Members")
-      # within("#character-5cf5679a915ecad153ab68fd") do
-        # within("#affiliation") do
+      within("#character-5cf5679a915ecad153ab68fd") do
+        within("#affiliation") do
           expect(page).to have_content("Fire Nation Navy")
-        # end
-      # end
+        end
+      end
     end
   end
 end
