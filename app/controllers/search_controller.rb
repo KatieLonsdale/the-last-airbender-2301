a5a1e549f5dc30cc444ae051829d6aaa0a426e49
Katie Lonsdale <katie952@gmail.com>
2023-05-26 14:18:00 -0600

    feat: add index aciton

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,15 @@
 class SearchController < ApplicationController
   def index
-    
+    @characters = []
+    5.times do 
+      page = 1
+      response = Faraday.new.get("https://last-airbender-api.fly.dev/api/v1/characters?page=#{page}&affiliation=#{params[:nation]}")
+      people = JSON.parse(response.body, symbolize_names: true)
+      people.each do |person|
+        @characters << Character.new(person)
+      end
+      page += 1
+    end
+    @characters
   end
 end
