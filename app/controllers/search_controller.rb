4725529b15e4c23f0f56ba7899f87c47a599e026
Katie Lonsdale <katie952@gmail.com>
2023-05-26 14:53:01 -0600

    fix: stop page from resetting each loop

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
     @characters = []
+    page = 1
     5.times do 
-      page = 1
       response = Faraday.new.get("https://last-airbender-api.fly.dev/api/v1/characters?page=#{page}&affiliation=#{params[:nation]}")
       people = JSON.parse(response.body, symbolize_names: true)
       people.each do |person|
