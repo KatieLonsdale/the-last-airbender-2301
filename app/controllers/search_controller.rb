669baa6e8b2e7918730b576f3dd0c3771c9a973f
Katie Lonsdale <katie952@gmail.com>
2023-05-26 14:59:47 -0600

    refactor: index action to use character facade

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @characters = []
-    page = 1
-    5.times do 
-      response = Faraday.new.get("https://last-airbender-api.fly.dev/api/v1/characters?page=#{page}&affiliation=#{params[:nation]}")
-      people = JSON.parse(response.body, symbolize_names: true)
-      people.each do |person|
-        @characters << Character.new(person)
-      end
-      page += 1
-    end
-    @characters
+    @characters = CharacterFacade.new.characters_by_nation(params[:nation])
   end
 end
