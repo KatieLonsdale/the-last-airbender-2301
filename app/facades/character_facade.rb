cd5bfd5177fce44950fd36f3c153f11c2b1d190a
Katie Lonsdale <katie952@gmail.com>
2023-05-26 15:13:46 -0600

    feat: add characters_by_nation method

--- app/facades/character_facade.rb
+++ app/facades/character_facade.rb
@@ -3,8 +3,7 @@ class CharacterFacade
     characters = []
     page = 1
     5.times do 
-      response = Faraday.new.get("https://last-airbender-api.fly.dev/api/v1/characters?page=#{page}&affiliation=#{nation}")
-      people = JSON.parse(response.body, symbolize_names: true)
+      people = CharacterService.characters_by_nation(nation, page)
       people.each do |person|
         characters << Character.new(person)
       end
