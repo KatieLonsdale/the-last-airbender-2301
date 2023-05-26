cb7b86eccffd20d4fc2fc6e35dbb09ce6dc08164
Katie Lonsdale <katie952@gmail.com>
2023-05-26 15:07:24 -0600

    test: add attributes tests

--- spec/poros/character_spec.rb
+++ spec/poros/character_spec.rb
@@ -21,11 +21,11 @@ RSpec.describe Character do
     it 'exists and has attributes' do
       expect(@character).to be_a Character
       expect(@character.id).to eq("5cf5679a915ecad153ab68cc")
-      # expect(@character.name)
-      # expect(@character.photo)
-      # expect(@character.enemies)
-      # expect(@character.allies)
-      # expect(@character.affiliation)
+      expect(@character.name).to eq("Afiko")
+      expect(@character.photo).to eq("https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128")
+      expect(@character.enemies).to eq(["Aang"])
+      expect(@character.allies).to eq(["Fire Nation"])
+      expect(@character.affiliation).to eq("Fire Nation")
     end
   end
 end
